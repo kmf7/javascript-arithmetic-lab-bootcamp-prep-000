52a0ad40c70eb8068f9381eb03893f9da2e07328
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-01-26 17:01:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function add(a,b){
   return a + b
 }
 
-function sybtract(a,b){
+function subtract(a,b){
   return a-b
 }
 
@@ -15,10 +15,12 @@ function divide(a,b){
 }
 
 function inc(n){
-  return n++
+  n ++
+  return n
 }
 
 function dec(n){
-  return n--
+  n--
+  return n
 }
 
